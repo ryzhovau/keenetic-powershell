9f64ca48c068e0b0946add17005674ec8b405542
Alexander Ryzhov <github@ryzhov-al.ru>
2021-03-08 19:25:03 +0300

    Update-Help: PlatyPs options

--- Update-Help.ps1
+++ Update-Help.ps1
@@ -2,6 +2,5 @@
 Import-Module platyPS
 Import-Module Keenetic -Force
 
-# New-MarkdownHelp -Module keenetic -NoMetadata -ModulePagePath "keenetic-wiki\README.MD" -OutputFolder .\keenetic-wiki -Force
-Update-MarkdownHelpModule -Path en-us -ModulePagePath "keenetic-wiki\README.MD" -RefreshModulePage -Verbose
-# Update-MarkdownHelp .\keenetic-wiki
+# New-MarkdownHelp -Module keenetic -NoMetadata -WithModulePage -ModulePagePath keenetic-wiki\Home.md -OutputFolder keenetic-wiki  -Force
+Update-MarkdownHelpModule -Path keenetic-wiki -ModulePagePath keenetic-wiki\Home.md
